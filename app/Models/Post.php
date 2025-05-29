1b482e9971e3bda2c2a283d765b4c5356ec8a71b
Taris Rafiqi <tarisrafiqi@gmail.com>
2025-05-29 11:01:51 +0700

    Configuration

---
+++ app/Models/Post.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function isLikedByUser(User $user)
+    {
+        return $this->likes()->where('user_id', $user->id)->exists();
+    }
+}
